76eae15df335a5fece04c49dfa4347d322a06ec1
Ramon Williams <ramnwor21@gmail.com>
2022-07-12 18:21:31 +0200

    Iteracion 5.2

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ const number3 = 2;
 if (number2/number1==2) {
     console.log("number2 dividido entre number1 es igual a 2");
   }
+  //iteracion 5.1
+  if (number1!==number2) {
+    console.log("number1 es estrictamente distinto a number2");
+  }
+  //iteracion 5.2
